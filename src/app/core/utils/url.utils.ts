ec539a01f945e86234742ff3fc2f3c9623e7d6f3
Jairo Filho <jairofilho79@gmail.com>
2025-10-15 15:41:13 -0300

    adding blob url

--- src/app/core/utils/url.utils.ts
+++ src/app/core/utils/url.utils.ts
@@ -1,11 +1,11 @@
-export function isValidHttpUrl(value: string | null | undefined): value is string {
+export function isValidPdfUrl(value: string | null | undefined): value is string {
   if (!value) {
     return false;
   }
 
   try {
     const url = new URL(value);
-    return url.protocol === 'http:' || url.protocol === 'https:';
+    return url.protocol === 'http:' || url.protocol === 'https:' || url.protocol === 'blob:';
   } catch {
     return false;
   }
